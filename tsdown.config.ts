77561198c3ae7b1a51c73bb23b919d3d0ab45703
Hēsperus <hesprs@outlook.com>
2026-06-01 19:44:32 +0800

    chore(release): prepare 0.0.2 release (#13)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
 	dts: true,
 	entry: ['src/index.ts', 'src/simple-suite.ts', 'src/di.ts'],
-	exports: true,
 	minify: true,
 	outExtensions: () => ({
 		dts: '.d.ts',
